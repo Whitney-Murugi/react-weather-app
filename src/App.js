b2ba22b0e87ae1a89f9914c1f867e91f831df86b
Whitney-Murugi <mmaitsimurugi@gmail.com>
2025-02-05 17:30:36 +0300

    fixed my code

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello student</h1>
+        <h1 class="text-danger">Weather Search☁</h1>
         <Weathers city="Tokyo"/>
       </header>
-      <p>This project is <a href="https://github.com/Whitney-Murugi/react-weather-app"> one-sourced in Github</a>  and <a href="https://singular-melomakarona-d9c979.netlify.app/">hosted in Netlify </a> </p>
+      <p>This project is <a href="https://github.com/Whitney-Murugi/react-weather-app"> one-sourced in Github</a>  and <a href="https://singular-melomakarona-d9c979.netlify.app/">hosted on Netlify </a> </p>
     </div>
   );
 }
--- src/weather.js
+++ src/weather.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import React from "react";
-import { ClipLoader } from "react-spinners";
 
 export default function Weathers(props) {
   function receiveWeather(response) {
@@ -13,15 +12,68 @@ export default function Weathers(props) {
   axios.get(ApiUrl).then(receiveWeather);
 
   return (
-    <ClipLoader
-      color="#FF00FF"
-      loading="true"
-      cssOverride={{
-  ClipLoader
-      }}
-      size={150}
-      aria-label="Loading Spinner"
-      data-testid="loader"
-    />
+    <div>
+      <form class="">
+        <div class="row">
+          <div class="col-8">
+            <input type="search" />
+          </div>
+          <div class="col-4">
+            <input type="submit" value="submit" class="btn btn-danger" />
+          </div>
+        </div>
+      </form>
+      <div class="row">
+        <div class="col-6">
+          <h2 class="fs-1">New York</h2>
+          <p class="fs-6">
+            Wednesday 10:41, few clouds
+            <br /> Humidity: 80%, Wind: 12.35km/h
+          </p>
+        </div>
+        <div class="col-6">
+          <p>
+            ⛅<span class="fs-1">7</span>℃
+          </p>
+        </div>
+      </div>
+      <div class="row" style={{ border: "2px solid red", borderRadius:"16px"}}>
+        <div class="col">
+          <p>
+            Mon <br />☀<br />
+            12℃
+          </p>
+        </div>
+        <div class="col">
+          <p>
+            Tue <br />⛅<br />
+            10℃
+          </p>
+        </div>
+        <div class="col">
+          {" "}
+          <p>
+            Wed <br />☀<br />
+            13℃
+          </p>
+        </div>
+        <div class="col">
+          {" "}
+          <p>
+            Thur <br />
+            🌦
+            <br />
+            11℃
+          </p>
+        </div>
+        <div class="col">
+          {" "}
+          <p>
+            Fri <br />⛈<br />
+            8℃
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
